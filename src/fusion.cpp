debab38dcab994bfdeb38dc21fba0ff71de8df83
millie <amelia.g.crowther@gmail.com>
2018-04-03 18:21:01 +0100

    set default parameters for fusion

--- src/fusion.cpp
+++ src/fusion.cpp
@@ -4,6 +4,14 @@ void
 fusion_t::fusion(){
     min_params_t ps;
 
+    // set defaults
+    ps.eta = 0.1f;
+    ps.omega_k = 0.5f;
+    ps.omega_s = 0.2f;
+    ps.gamma = 0.1f;
+    ps.epsilon = 0.00005f;
+    ps.threshold = 0.1f;
+
     while (true){
         sdf_t * sdf = get_next_sdf();
         
